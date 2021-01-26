bdbb59980b5c3bda72628bdb480bb5bbe4708675
dejanagrujic <39495830+dejanagrujic@users.noreply.github.com>
2021-01-26 23:42:39 +0100

    Switch to next player

--- script.js
+++ script.js
@@ -1,46 +1,55 @@
 'use strict';
 
 // Selecting elements
+const player0 = document.querySelector('.player-0');
+const player1 = document.querySelector('.player-1');
 const score0 = document.querySelector('#score-0');
 const score1 = document.getElementById('score-1');
 const dice = document.querySelector('.dice');
 const current0 = document.getElementById('current-0');
 const current1 = document.getElementById('current-1');
+const btnNew = document.querySelector('.btn-new');
+const btnRoll = document.querySelector('.btn-roll');
+const btnHold = document.querySelector('.btn-hold');
 
 //Starting conditions
 score0.textContent = 0;
 score1.textContent = 0;
 let currentScore = 0;
+let activePlayer = 0;
+const scores = [0, 0];
 dice.classList.add('hidden');
 
-document.querySelector('.btn-roll').addEventListener('click', function () {
-  let diceNumber = Math.trunc(Math.random() * 6) + 1;
-  console.log('diceNumber');
+//Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  //Generate a random number between 1-6
+  const diceNumber = Math.trunc(Math.random() * 6) + 1;
+  //console.log('diceNumber');
 
-  switch (diceNumber) {
-    case 1:
-      dice.classList.remove('hidden');
-      document.querySelector('.dice').src = 'dice-1.png';
-      break;
-    case 2:
-      dice.classList.remove('hidden');
-      document.querySelector('.dice').src = 'dice-2.png';
-      break;
-    case 3:
-      dice.classList.remove('hidden');
-      document.querySelector('.dice').src = 'dice-3.png';
-      break;
-    case 4:
-      dice.classList.remove('hidden');
-      document.querySelector('.dice').src = 'dice-4.png';
-      break;
-    case 5:
-      dice.classList.remove('hidden');
-      document.querySelector('.dice').src = 'dice-5.png';
-      break;
-    case 6:
-      dice.classList.remove('hidden');
-      document.querySelector('.dice').src = 'dice-6.png';
-      break;
+  //Display dice
+  dice.classList.remove('hidden');
+  dice.src = `dice-${diceNumber}.png`;
+
+  //Check for rolled = 1
+  if (diceNumber !== 1) {
+    //display new score
+    currentScore += diceNumber;
+    document.getElementById(
+      `current-${activePlayer}`
+    ).textContent = currentScore;
+
+    //current0.textContent = currentScore;
+  } else {
+    //switch to next player
+    currentScore = 0;
+    document.getElementById(
+      `current-${activePlayer}`
+    ).textContent = currentScore;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+
+    if (activePlayer === 1) {
+      player0.classList.toggle('player-active');
+      player1.classList.toggle('player-active');
+    }
   }
 });
